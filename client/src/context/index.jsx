54e11868a4d8ddefbcdcc20987b2cc4cede9c42b
vishh2dev <vishh2dev@gmail.com>
2023-04-22 21:20:17 +0530

    added web3 context to the app

---
+++ client/src/context/index.jsx
@@ -0,0 +1,46 @@
+import React, { useContext, createContext} from 'react'
+
+import{ useAddress, useContract, useMetamask, useContractWrite} from '@thirdweb-dev/react'
+import { ethers } from 'ethers';
+
+const StateContext = createContext()
+
+export const StateContextProvider = ({children}) =>{
+    const { contract } = useContract('0x16eDF7a89935E7655809b4c9eda8FBf526345739')
+    const {mutateAsync: createCampaign} = useContractWrite(contract, 'createCampaign')
+
+    const address = useAddress()
+    const connect = useMetamask()
+
+    const publishCampaign = async(form) =>{
+        try {
+            const data = await createCampaign([
+                address, //owner
+                form.title,// title
+                form.description, //description
+                form.target,
+                new Date(form.deadline).getTime(), //deadline
+                form.image
+            ])
+            console.log('contract call sucess',data)
+        } catch (error) {
+            console.log('contract call failed',error)
+        }
+        
+    }
+    return(
+        <StateContext.Provider
+            value={{
+                address,
+                contract,
+                connect,
+                createCampaign: publishCampaign
+            }}
+        >
+            {children}
+        </StateContext.Provider>
+    )
+    
+}
+
+export const useStateContext = () => useContext(StateContext) 
