cc8de383bf51905908bb5cdeaedbbaf8d3aa53d3
vishh2dev <vishh2dev@gmail.com>
2023-04-22 21:20:38 +0530

    added a connect button to connect to metmask wallet

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,12 +3,14 @@ import {Link, useNavigate} from 'react-router-dom'
 import CustomButton from './CustomButton'
 import { logo, menu, search, thirdweb } from '../assets'
 import {navlinks} from '../constants'
+import { useStateContext } from '../context'
 
 const Navbar = () => {
   const navigate = useNavigate();
   const [isActive, setIsActive] = useState('dashboard');
   const [toggleDrawer, setToggleDrawer] = useState(false)
-  const address = '0xad'
+  const { address, connect} = useStateContext()
+  
   return (
     <div className='flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6'>
       <div className='lg:flex-1 flex flex-row max-w-[458px] py-2 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]'>
@@ -24,7 +26,7 @@ const Navbar = () => {
           styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
           handleClick = {() =>{
             if(address) navigate('create-campaign')
-            else 'connect()'
+            else connect()
           }}
         />
 
@@ -37,7 +39,7 @@ const Navbar = () => {
       {/*small screen navigation*/}
       <div className='sm:hidden flex justify-between items-center relative'>
         <div className='w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer'>
-              <img src={thirdweb} alt='user' className='w-[60%] h-[60%] object-contain' />
+              <img src={logo} alt='user' className='w-[60%] h-[60%] object-contain' />
         </div>
         <img 
           src={menu}
@@ -71,7 +73,7 @@ const Navbar = () => {
               styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
               handleClick = {() =>{
                 if(address) navigate('create-campaign')
-                else 'connect()'
+                else connect()
               }}
             />
           </div>
