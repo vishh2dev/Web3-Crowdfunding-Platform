455e9f1732bc24338bbb90d5c182dc94eebedbdc
vishh2dev <vishh2dev@gmail.com>
2023-04-24 22:26:09 +0530

    Fetch the campaigns and display in the homepage

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -28,13 +28,31 @@ export const StateContextProvider = ({children}) =>{
         }
         
     }
+
+    const getCampaigns = async () =>{
+        const campaigns = await contract.call('getCampaigns') 
+
+        const parsedCampaigns = campaigns.map((campaign,i) => ({
+            owner: campaign.owner,
+            title: campaign.title,
+            description: campaign.description,
+            target: ethers.utils.formatEther(campaign.target.toString()),
+            deadline: campaign.deadline.toNumber(),
+            amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
+            image: campaign.image,
+            pId: i
+        }))
+        console.log(parsedCampaigns)
+        return parsedCampaigns
+    }
     return(
         <StateContext.Provider
             value={{
                 address,
                 contract,
                 connect,
-                createCampaign: publishCampaign
+                createCampaign: publishCampaign,
+                getCampaigns
             }}
         >
             {children}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React,{ useEffect,useState} from 'react'
 
+import { useStateContext } from '../context'
 const Home = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [campaigns, setCampaigns] = useState([])
+
+  const {address,contract, getCampaigns} = useStateContext()
+
+  const fetchCampaigns = async() =>{
+    setIsLoading(true)
+    const data = await getCampaigns()
+    setCampaigns(data)
+    setIsLoading(false)
+  }
+  useEffect(() =>{
+   if(contract) fetchCampaigns()
+  },[contract,address])
+
   return (
     <div>
       Home
